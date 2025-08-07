c677db8b2f8b3c7d464056db8f970d1554491468
tanvikohale <tanvikohale23@gmail.com>
2025-08-07 18:40:45 +0530

    Add new folders

---
+++ Input_Output/script.js
@@ -0,0 +1,50 @@
+
+console.log("hello js using external script.js page !")
+
+// window.alert("hello ! I am Soniya")
+
+// Tasks 
+
+// ARITHMATIC OPERATOR
+let num1 = 10
+let num2 = 20
+
+let sum = num1 + num2
+// sum
+console.log(sum)
+// multiplication
+console.log(num1 * num2)
+// division
+console.log(num1 / num2)
+// substraction
+console.log(num1 - num2)
+// modulous
+console.log(num1 % num2)
+
+
+// ASSIGNMENTS OPERATOR
+let data = 50
+
+console.log (data += 10) 
+console.log (data -= 10)
+console.log (data *= 10) 
+console.log (data /= 10) 
+console.log (data %= 10)
+
+// COMPARISON OPERATOR
+let data1 = 100
+
+console.log(data1 > 90)
+console.log(data1 < 90)
+console.log(data1 <= 100)
+console.log(data1 >= 120)
+console.log(data1 != 100)
+console.log(data1 == 900)
+console.log(data1 === 100)
+
+// simple example
+
+let name = window.prompt("Enter Your Name")
+let address = window.prompt("Enter Your Address here")
+
+console.log( "Hello ! I am " + name + " I am from " + address + ".")
